622bfd3346bdcf3fe5e49e6a29f6c0d5692b783e
LarsVerschoor <larsverschoor2@gmail.com>
2024-06-17 15:30:24 +0200

    idk

--- src/js/ui.js
+++ src/js/ui.js
@@ -3,32 +3,21 @@ import { Actor, Keys, Vector, Engine, Label, Font, Color } from "excalibur";
 export class UI extends Actor {
     ammoLabel;
     hpLabel;
-    uiX;
-    uiY;
     onInitialize(engine) {
         this.ammoLabel = new Label({
             text: 'ammo 0',
             font: new Font({
                 family: 'impact',
-                pos: new Vector(0, 0),
                 size: 24,
                 color: Color.Red
             }),
-
+            pos: new Vector(100, 100)
 
         });
-        engine.add(this.ammoLabel)
+        this.addChild(this.ammoLabel)
     }
     updateScore(ammo) {
         this.ammoLabel.text = `ammo ${ammo}`;
     }
-    updatePosition(playerX, playerY) {
-        this.uiX = playerX
-        this.uiY = playerY
-        console.log(this.uiX, this.uiY)
-    }
-    onPreUpdate(engine) {
-        engine.add(this.ammoLabel)
-        pos: new Vector(this.uiX, this.uiY)
-    }
+
 }
