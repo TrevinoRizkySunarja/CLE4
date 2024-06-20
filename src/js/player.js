ce5bdbf45085cfa63ad279261a51959aac5ae5f2
TrevinoRizkySunarja <trevprive123@gmail.com>
2024-06-20 10:01:27 +0200

    Player.js bug fix.

--- src/js/player.js
+++ src/js/player.js
@@ -6,94 +6,92 @@ import { UI } from './ui';
 import { Zombie } from './zombie';
 import { Game } from './game';
 
-
 class Player extends Actor {
-	ui;
-	bulletDirection = new Vector(1, 0);
-	speed = 70;
-	hp = 3;
-	pistol;
+    ui;
+    bulletDirection = new Vector(1, 0);
+    speed = 70;
+    hp = 3;
+    pistol;
 
-	constructor({pos}) {
-		super({width: 14, height: 30});
-	constructor({ pos }) {
-		super({ width: 20, height: 30 });
-		this.pos = pos;
-		this.scale = new Vector(1.5, 1.5);
-	}
+    constructor({ pos }) {
+        super({ width: 20, height: 30 });
+        this.pos = pos;
+        this.scale = new Vector(1.5, 1.5);
+    }
 
-	onInitialize() {
-		this.graphics.use(Resources.PlayerFullHealthRight.toSprite());
-		this.ui = new UI();
-		this.addChild(this.ui);
-		this.pistol = new Pistol();
-		this.addChild(this.pistol);
-		this.on('collisionstart', (event) => this.hitEnemy(event));
-	}
+    onInitialize() {
+        this.graphics.use(Resources.PlayerFullHealthRight.toSprite());
+        this.ui = new UI();
+        this.addChild(this.ui);
+        this.pistol = new Pistol();
+        this.addChild(this.pistol);
+        this.on('collisionstart', (event) => this.hitEnemy(event));
+    }
 
-	onPreUpdate(engine, delta) {
-		let vX = 0;
-		let vY = 0;
-		if (engine.input.keyboard.isHeld(Keys.S)) {
-			vY += this.speed;
-			this.graphics.use(Resources.PlayerFullHealthDown.toSprite());
-			this.bulletDirection.setTo(0, 1);
-		}
-		//Naar boven
-		if (engine.input.keyboard.isHeld(Keys.W)) {
-			vY -= this.speed;
-			this.graphics.use(Resources.PlayerFullHealthUp.toSprite());
-			this.bulletDirection.setTo(0, -1);
-		}
-		//Naar rechts
-		if (engine.input.keyboard.isHeld(Keys.D)) {
-			vX += this.speed;
-			this.graphics.use(Resources.PlayerFullHealthRight.toSprite());
-			this.bulletDirection.setTo(1, 0);
-		}
-		//Naar links
-		if (engine.input.keyboard.isHeld(Keys.A)) {
-			vX -= this.speed;
-			this.graphics.use(Resources.PlayerFullHealthLeft.toSprite());
-			this.bulletDirection.setTo(-1, 0);
-		}
+    onPreUpdate(engine, delta) {
+        let vX = 0;
+        let vY = 0;
+        if (engine.input.keyboard.isHeld(Keys.S)) {
+            vY += this.speed;
+            this.graphics.use(Resources.PlayerFullHealthDown.toSprite());
+            this.bulletDirection.setTo(0, 1);
+        }
+        // Naar boven
+        if (engine.input.keyboard.isHeld(Keys.W)) {
+            vY -= this.speed;
+            this.graphics.use(Resources.PlayerFullHealthUp.toSprite());
+            this.bulletDirection.setTo(0, -1);
+        }
+        // Naar rechts
+        if (engine.input.keyboard.isHeld(Keys.D)) {
+            vX += this.speed;
+            this.graphics.use(Resources.PlayerFullHealthRight.toSprite());
+            this.bulletDirection.setTo(1, 0);
+        }
+        // Naar links
+        if (engine.input.keyboard.isHeld(Keys.A)) {
+            vX -= this.speed;
+            this.graphics.use(Resources.PlayerFullHealthLeft.toSprite());
+            this.bulletDirection.setTo(-1, 0);
+        }
 
-		if (vX === 0 && vY === 0) this.vel = new Vector(0, 0);
-		else this.vel = Vector.fromAngle(Math.atan2(vY, vX)).scale(80);
+        if (vX === 0 && vY === 0) this.vel = new Vector(0, 0);
+        else this.vel = Vector.fromAngle(Math.atan2(vY, vX)).scale(80);
 
-		const spacePressed = engine.input.keyboard.isHeld(Keys.Space);
-		if (engine.input.keyboard.isHeld(Keys.R)) {
-			this.pistol.reload(engine.currentScene);
-		}
+        const spacePressed = engine.input.keyboard.isHeld(Keys.Space);
+        if (engine.input.keyboard.isHeld(Keys.R)) {
+            this.pistol.reload(engine.currentScene);
+        }
 
-		if (spacePressed && !this.prevSpacePressed) {
-			this.pistol.shoot(engine.currentScene, this.bulletDirection);
-		}
-		this.prevSpacePressed = spacePressed;
-	}
+        if (spacePressed && !this.prevSpacePressed) {
+            this.pistol.shoot(engine.currentScene, this.bulletDirection);
+        }
+        this.prevSpacePressed = spacePressed;
+    }
 
-	//functie dat detecteert dat de player en zombie elkaar hebben gehit
-	hitEnemy(event) {
-		if (event.other instanceof Zombie) {
-			this.hp--;
-			console.log(this.hp);
-			if (this.hp === 0) this.kill();
-			this.ui.updateHp(this.hp);
-		}
-	}
-	getDistance(x, y) {
-		const deltaX = x - this.pos.x;
-		const deltaY = y - this.pos.y;
+    // functie dat detecteert dat de player en zombie elkaar hebben gehit
+    hitEnemy(event) {
+        if (event.other instanceof Zombie) {
+            this.hp--;
+            console.log(this.hp);
+            if (this.hp === 0) this.kill();
+            this.ui.updateHp(this.hp);
+        }
+    }
 
-		return Math.sqrt(Math.pow(deltaX, 2) + Math.pow(deltaY, 2));
-	}
+    getDistance(x, y) {
+        const deltaX = x - this.pos.x;
+        const deltaY = y - this.pos.y;
 
-	getAngle(x, y) {
-		const deltaX = this.pos.x - x;
-		const deltaY = this.pos.y - y;
+        return Math.sqrt(Math.pow(deltaX, 2) + Math.pow(deltaY, 2));
+    }
 
-		return Math.atan2(deltaY, deltaX);
-	}
+    getAngle(x, y) {
+        const deltaX = this.pos.x - x;
+        const deltaY = this.pos.y - y;
+
+        return Math.atan2(deltaY, deltaX);
+    }
 }
 
 export { Player };
